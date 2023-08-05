7bc460f611a1fde85ecff078e5bb211bf0ba901c
Alexey <alexeylutv@vk.com>
2023-08-05 13:06:06 +0300

    add some solutions

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -2,3 +2,25 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m) return n;
+    else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int n = Prompt("Input N: ");
+if (n < 1)
+{
+    Console.WriteLine("Ввдите положительное число!");
+    return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
